e4b35a5202bb917a09e3b3affb18b6338b765afa
mekhtear <117963714+mekhtear@users.noreply.github.com>
2023-04-03 23:53:34 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 // Imports
 import userRoutes from './users.js';
+import transactionRoutes from './transactions.js';
+import budgetRoutes from './budgets.js';
 
 // Build a constructor to glue routes together
 const constructorMethod = (app) => {
     // Configure the app to use all routes --> app.use('/<route>', <importedFile>);
     app.use('/users', userRoutes);
+      app.use('/transactions', transactionRoutes);
+    app.use('/budgets', budgetRoutes);
     
     // Set the default route
     app.use('*', (req, res) => {
