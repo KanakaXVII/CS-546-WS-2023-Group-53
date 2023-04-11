38bc49ed3e923c17488823b183bedb0fb21dd7e0
mekhtear <117963714+mekhtear@users.noreply.github.com>
2023-04-11 19:11:54 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import configRoutes from './routes/index.js';
 import exphbs from 'express-handlebars';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import forgotPasswordRouter from './routes/forgot-password';
 
 // Create variables for the path directory
 const __filename = fileURLToPath(import.meta.url);
@@ -16,6 +17,7 @@ const _static = express.static(__dirname + '/public');
 app.use('/public', _static);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/', forgotPasswordRouter);
 
 // Configure the template engine
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
